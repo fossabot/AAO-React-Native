841b4e569cbb8d56d774a0d794bca72f60f7729c
Drew Volz <volz.drew@gmail.com>
2018-02-05 01:24:49 -0700

    Weather images

---
+++ images/weather-images.js
@@ -0,0 +1,19 @@
+// @flow
+export const weatherImages = {
+  'partly-cloudy-day': require('./weather/partly-cloudy-day.png'),
+  'partly-cloudy-night': require('./weather/partly-cloudy-night.png'),
+  'clear-day': require('./weather/clear-day.png'),
+  'clear-night': require('./weather/clear-night.png'),
+  'rain': require('./weather/rain.png'),
+  'preip': require('./weather/precip.png'),
+  'snow': require('./weather/snow.png'),
+  'sleet': require('./weather/sleet.png'),
+  'wind': require('./weather/wind.png'),
+  'fog': require('./weather/fog.png'),
+  'cloudy': require('./weather/cloudy.png'),
+  'hail': require('./weather/hail.png'),
+  'thunderstorm': require('./weather/thunderstorm.png'),
+  'tornado': require('./weather/tornado.png'),
+  'meteor-shower': require('./weather/meteor-shower.png'),
+  default: require('./weather/default.png')
+}
